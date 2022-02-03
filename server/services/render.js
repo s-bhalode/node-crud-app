24719ebdfb797d7766acc42b9f4767dc69887b19
Simran Bhalode <simran.bhalode@gmail.com>
2022-02-03 23:28:34 +0530

    basic-crud-app-done

---
+++ server/services/render.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+
+exports.homeRoutes = (req, res) => {
+    // MAke a get request to/api/users
+    axios.get('http://localhost:3000/api/users')
+        .then(response => {
+            res.render("index", {users : response.data});
+        })
+        .catch(err => {
+            res.send(err);
+        })
+
+
+    
+}
+
+exports.add_user = (req, res) => {
+    res.render("add_user");
+}
+
+exports.update_user = (req, res) => {
+    axios.get('http://localhost:3000/api/users', {params : {id : req.query.id}})
+        .then(userdata => {
+            res.render("update_user", {user : userdata.data})
+        })
+        .catch(err => {
+            res.send(err);
+        })
+}
